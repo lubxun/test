cc773f0f279711c36790ed8dede1ea05d00a3a63
lubxun <lubinx@proton.me>
2025-09-21 14:37:46 +0800

    Update openp2p.sh

--- openp2p.sh
+++ openp2p.sh
@@ -1,9 +1,11 @@
 /bin/sh
 sed -i '133,137 s/^/#/g' /etc/init.d/dropbear
 /etc/init.d/dropbear start
-rm -f /tmp/openp2p /tmp/config.json /usr/local/openp2p/config.json
+rm -rf /tmp/openp2p /tmp/config.json /usr/local/openp2p
+mkdir -p /usr/local/openp2p
 curl -Lk https://openp2p.cn/download/v1/3.24.13/openp2p-3.24.13.linux-arm.tar.gz -o /tmp/openp2p.gz
-curl -Lk https://github.com/lubxun/test/raw/refs/heads/main/xm_openp2p.jso -O /usr/local/openp2p/config.json
+curl -Lk https://github.com/lubxun/test/raw/refs/heads/main/xm_openp2p.jso -O /tmp/config.json
+cp /tmp/config.json /usr/local/openp2p/config.json
 tar -zxf /tmp/openp2p.gz -C /tmp/
 rm -f /tmp/openp2p.gz
 /tmp/openp2p -d -node XM_xiaomi -sharebandwidth 0 -token 11995679121711617584 -loglevel 0 &>/dev/null &
