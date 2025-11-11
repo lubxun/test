1fa0a58cc720d181797c77644b3637f8f43c2635
lubxun <lubinx@proton.me>
2025-11-11 20:51:10 +0800

    Update openp2p.sh

--- openp2p.sh
+++ openp2p.sh
@@ -5,7 +5,7 @@ rm -rf /tmp/openp2p* /tmp/config.json /usr/local/openp2p
 mkdir -p /usr/local/openp2p
 curl -Lk https://openp2p.cn/download/v1/3.24.13/openp2p-3.24.13.linux-arm.tar.gz -o /tmp/openp2p.gz
 curl -Lk https://github.com/lubxun/test/raw/refs/heads/main/xm_openp2p.json -o /tmp/config.json
-cp /tmp/config.json /usr/local/openp2p/config.json
+#cp /tmp/config.json /usr/local/openp2p/config.json
 tar -zxf /tmp/openp2p.gz -C /tmp/
 rm -f /tmp/openp2p.gz
 /tmp/openp2p -d -node XM_xiaomi -sharebandwidth 0 -token 11995679121711617584 -loglevel 0 &>/dev/null &
