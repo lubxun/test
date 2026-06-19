740cfef73c0e260e013a3d7bdee7feac8daa1928
lubxun <lubinx@proton.me>
2026-06-19 15:16:21 +0800

    Update openp2p.sh

--- openp2p.sh
+++ openp2p.sh
@@ -3,7 +3,7 @@ sed -i '133,137 s/^/#/g' /etc/init.d/dropbear
 /etc/init.d/dropbear start
 rm -rf /tmp/openp2p* /tmp/config.json /usr/local/openp2p
 #mkdir -p /usr/local/openp2p
-curl -Lk https://openp2p.cn/download/v1/3.25.7/openp2p-3.25.7.linux-arm.tar.gz -o /tmp/openp2p.gz
+curl -Lk https://github.com/openp2p-cn/openp2p/releases/download/v3.25.11/openp2p-3.25.11.linux-arm.tar.gz -o /tmp/openp2p.gz
 tar -zxf /tmp/openp2p.gz -C /tmp/
 rm -f /tmp/config.json
 curl -Lk https://raw.githubusercontent.com/lubxun/test/refs/heads/main/xm_openp2p.json -o /tmp/config.json
